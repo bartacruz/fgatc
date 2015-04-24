7fe698c53e1d99c1d8986f0b113716e98b6eb4d6
julio <jscruz@xtech.com.ar>
2015-04-24 18:38:42 -0300

    Importer: use bulk_create and decode/encode iso8559-1 to utf8

--- fgserver/tools/airport_importer.py
+++ fgserver/tools/airport_importer.py
@@ -7,10 +7,18 @@ Created on Apr 24, 2015
 from fgserver.models import Airport, Runway
 from fgserver.helper import normalize
 
-def rwy_from_aptdat(airport,line):
+
+def rwys_from_aptdat(airport,line):
+    ''' 
+    Decodes a runway from FG apt.dat and his opposite too 
+        - airport is an fgserver.Airport instance
+        - line is an array with the line of apt.dat already splitted
+    returns an array with the runways suited to use in Runway.objects.bulk_create
+    '''
+    runways=[]
     rwy = Runway()
     rname = line[3].replace('x','')
-    # Runway
+    
     #r,created = Runway.objects.get_or_create(airport__icao=airport.icao, name=designation)
     r = Runway(airport=airport, name=rname)
     r.altitude=airport.altitude
@@ -19,9 +27,11 @@ def rwy_from_aptdat(airport,line):
     r.lon = line[2]
     r.length = line[5]
     r.width = line[8]
-    r.save()
-    print r.__dict__
+    runways.append(r)
+    #print r.__dict__
+    ''' If it's not an helipad, calculate the opposite runway '''
     if not r.name.startswith('H'):
+        
         rbearing = normalize(float(r.bearing)+180)
         rname = str(int(normalize(float(r.name[:2])*10+180)/10))
         if r.name[2:3]=='L':
@@ -30,14 +40,17 @@ def rwy_from_aptdat(airport,line):
             rname= rname + "L"
              
         rinv = Runway(airport=airport, name=rname)
-        rinv.altitude=airport.altitude
         rinv.bearing = rbearing
-        rinv.lat = line[1]
-        rinv.lon = line[2]
-        rinv.length = line[5]
-        rinv.width = line[8]
-        rinv.save()
-        print "I",rinv.__dict__
+        ''' The original runway lat/lon mark the center of the rwy, which is the same for the opposite.''' 
+        rinv.lat = r.lat
+        rinv.lon = r.lon
+        ''' ditto for length, altitude and with. ''' 
+        rinv.length = r.length
+        rinv.altitude=r.altitude
+        rinv.width = r.width
+        runways.append(rinv)
+        #print "I",rinv.__dict__
+    return runways
 
 def import_apts(file):
     import gzip
@@ -45,6 +58,7 @@ def import_apts(file):
     cont = True
     while cont:
         line=f.readline()
+        line = line.decode('iso-8859-1').encode('utf8')
         if line.startswith('1 '):
             line = line.strip()
             #print line
@@ -57,59 +71,28 @@ def import_apts(file):
                 airport.altitude=aline[1]
                 airport.save()
                 done = False
-            else:
-                done=True
-            while not done:
-                l = f.readline()
-                if l == '\n':
-                    done=True
-                    break
-                l = l.strip()
-                al = l.split(None)
-                if l.startswith('10 '):
-                    if not airport.lat:
+                runways=[]
+                while not done:
+                    l = f.readline()
+                    if l == '\n':
+                        done=True
+                        break
+                    l = l.strip()
+                    al = l.split(None)
+                    if l.startswith('10 '):
+                        if not airport.lat:
+                            airport.lat = al[1]
+                            airport.lon = al[2]
+                        if al[3] != 'xxx':
+                            runways += rwys_from_aptdat(airport, al)
+                        #TODO: generate runways
+                    if l.startswith('14 '):
+                        #TODO: Generate controller
+                        print "found tower %s " % al[5]
                         airport.lat = al[1]
                         airport.lon = al[2]
-                    if al[3] != 'xxx':
-                        designation = al[3].replace('x','')
-                        # Runway
-                        #r,created = Runway.objects.get_or_create(airport__icao=airport.icao, name=designation)
-                        r = Runway(airport=airport, name=designation)
-                        created = True
-                        if created:
-                            r.altitude=airport.altitude
-                            r.bearing = al[4]
-                            r.lat = al[1]
-                            r.lon = al[2]
-                            r.length = al[5]
-                            r.width = al[8]
-                            r.save()
-                            print r.__dict__
-                            if not r.name.startswith('H'):
-                                rbearing = normalize(float(r.bearing)+180)
-                                rname = str(int(normalize(float(r.name[:2])*10+180)/10))
-                                if r.name[2:3]=='L':
-                                    rname= rname + "R"
-                                if r.name[2:3]=='R':
-                                    rname= rname + "L"
-                                     
-                                rinv = Runway(airport=airport, name=rname)
-                                rinv.altitude=airport.altitude
-                                rinv.bearing = rbearing
-                                rinv.lat = al[1]
-                                rinv.lon = al[2]
-                                rinv.length = al[5]
-                                rinv.width = al[8]
-                                rinv.save()
-                                print "I",rinv.__dict__
-                            
-                    #TODO: generate runways
-                if l.startswith('14 '):
-                    #TODO: Generate controller
-                    print "found tower %s " % al[5]
-                    airport.lat = al[1]
-                    airport.lon = al[2]
-            airport.save()
-            print airport.icao, airport.name, airport.lat, airport.lon,airport.altitude
+                airport.save()
+                Runway.objects.bulk_create(runways)
+                print airport.icao, airport.name, airport.lat, airport.lon,airport.altitude, len(runways)
 
 import_apts("../data/apt.dat.gz")
