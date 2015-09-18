2126c6992a132855d2c4576ef788a686a30f0e22
julio <bartacruz@gmail.com>
2015-09-18 13:52:04 -0300

    Code cleanup

--- fgserver/map/views.py
+++ fgserver/map/views.py
@@ -21,26 +21,17 @@ def map_view(request):
                     context_instance=RequestContext(request))
 
 def aircrafts(request):
-    lat = float(request.REQUEST.get('lat'))
-    lon = float(request.REQUEST.get('lon'))
-    zoom = request.REQUEST.get('zoom',1)
-    center = Position(lat,lon,0)
     aircrafts = Aircraft.objects.filter(state__gte=1)
     acfts = []
     for aircraft in aircrafts:
-        dist = get_distance(center, aircraft.get_position())
-        #print "map: dist=",dist,aircraft
         acfts.append(aircraft)
-        #callsigns.append({"callsign":aircraft.callsign, "lat": float(aircraft.lat),"lon":float(aircraft.lon)})
     d = json.loads(serialize('json',acfts ))
-    #print d    
     return HttpResponse(json.dumps({'aircrafts': d,}), mimetype='application/javascript;charset=utf-8"')
 
 def flightplan(request):
     callsign = request.REQUEST.get('callsign')
     wps = WayPoint.objects.filter(flightplan__aircraft__callsign=callsign)
     d = json.loads(serialize('json',wps ))
-    print callsign, d    
     return HttpResponse(json.dumps({'waypoints': d}), mimetype='application/javascript;charset=utf-8"')
 
     
