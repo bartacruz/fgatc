65fb10c267b53df7effe37274ee46a49080a039b
julio <bartacruz@gmail.com>
2019-03-11 10:19:24 -0300

    FGTracker interface

--- fgserver/urls.py
+++ fgserver/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^map/', include('fgserver.map.urls'))
+    url(r'^map/', include('fgserver.map.urls')),
+    url(r'^tracker/', include('fgserver.tracker.urls')),
 )
